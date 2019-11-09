d7537bdc1f69e07153799fad49d0e6f789ac1022
Cheng-Rong Lai <jimrong79@gmail.com>
2019-11-08 18:14:21 -0800

    Updates on dvp adjustment

--- yahoo_dfs_optimizer.py
+++ yahoo_dfs_optimizer.py
@@ -3,33 +3,107 @@ import numpy as np
 import pandas as pd
 import statistics 
 import sys
-import request
+from collections import defaultdict
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
 
 
 dvp_list = pd.read_html('https://basketballmonster.com/dfsdvp.aspx')
 dvp = dvp_list[0]
 
+def getting_dvp_by_pos():
+    dvp_dict = {}
+    
+    #driver = webdriver.Chrome(r"C:\Users\jimro\AppData\Local\Programs\Python\Python37-32\Lib\site-packages\selenium\webdriver\chromedriver_win32\chromedriver.exe")
+    driver = webdriver.Chrome(r"C:\Users\710453\AppData\Local\Programs\Python\Python36-32\Lib\site-packages\selenium\webdriver\chromedriver_win32\chromedriver.exe")
+    url = "https://basketballmonster.com/DailyEaseRankings.aspx"
+    
+    option_dict = {"3": "C", "4": "PG", "5": "SG", "6": "SF", "7": "PF"}
+    
+    driver.get(url)
+    
+    for option, pos in option_dict.items():
+        
+        
+        element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//select[@name='ctl00$ContentPlaceHolder1$PositionDropDownList']/option[@value='" + option + "']"))
+        )
+        element.click()
+        WebDriverWait(driver, 3).until(
+            EC.text_to_be_present_in_element(
+                (By.XPATH, "//select[@name='ctl00$ContentPlaceHolder1$PositionDropDownList']/option[@selected='selected']"),
+                pos
+            )
+        )
+
+        page = driver.page_source
+        soup = BeautifulSoup(page, 'html.parser')
+
+        whole_table = soup.findAll('tr', limit = 2)[1]
+        headers = [th.getText() for th in whole_table.findAll('tr', limit = 1)[0].findAll('td')]
+        rows = whole_table.findAll('tr')[1:]
+        dvp_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
+
+        for i in range(len(dvp_stats)):
+            dvp_stats[i][0] = dvp_stats[i][0].replace("vs", "")
+            dvp_stats[i][0] = dvp_stats[i][0].strip()
+
+        dvp_stats_df = pd.DataFrame(dvp_stats, columns = headers)
+        
+        dvp_dict[pos] = dvp_stats_df
+
+        print (dvp_stats_df.head(31))
+
+    driver.quit()
+    return dvp_dict
 
 
-per_game_list = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2020_per_game.html")
-per_game = per_game_list[0]
-per_game.sort_values(by = "Tm", inplace = True)
+def get_per_game_stats(team_opp, inactive_players):
+    
+    per_game_list = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2020_per_game.html")
+    per_game = per_game_list[0]
+    per_game.sort_values(by = "Tm", inplace = True)
+    
+    # Adding opponent column
+    per_game['Opponent'] = per_game['Tm'].map(team_opp)
+    per_game['Injured'] = per_game['Player'].map(inactive_players)
+
+    # Dropping players not playing today
+    per_game = per_game[per_game.Injured.isnull()]
+    per_game = per_game[per_game.Opponent.notnull()]
+
+    fan_pts_dict = {'PTS':1, 'TRB':1.2, 'AST':1.5, 'STL':3, 'BLK':3, 'TOV':-1}
+          
+    
+    return per_game
 
 
-fan_pts_dict = {'PTS':1, 'TRB':1.2, 'AST':1.5, 'STL':3, 'BLK':3, 'TOV':-1}
+
+
 
 
 def main():
-
+    
+    team_opp = {}
+    inactive_players = {}
+    
     exclude_list_last_name =  []
     exclude_list_time = []
     late_game = False
     if late_game:
         exclude_list_time = ['7:00PM EDT', '7:30PM EDT']
-    players = import_contest_data()
-    players = adjust_fppg_by_pace(players)
-    players = lock_unlock_players(players, exclude_players = exclude_list_last_name, exclude_time = exclude_list_time)
-    build_lineup(players)
+    yahoo_contest = import_contest_data(team_opp, inactive_players)
+    players = get_per_game_stats(team_opp, inactive_players)
+    dvp_dict = getting_dvp_by_pos()
+    #players = adjust_fppg_by_pace(players)
+    #players = lock_unlock_players(players, exclude_players = exclude_list_last_name, exclude_time = exclude_list_time)
+    #build_lineup(players)
 
 
 
@@ -50,10 +124,10 @@ def adjust_fppg_by_pace(players_df):
     """
 
     # mapping team names to yahoo format
-    team_name_transfer_dict_espn = {"LA Clippers" : "LAC", "San Antonio": "SA", "Phoenix": "PHO", "Atlanta":"ATL", "Dallas":"DAL", "Portland":"POR", 
-                                    "Minnesota":"MIN", "New Orleans":"NO", "Detroit":"DET", "Brooklyn":"BKN", "Toronto":"TOR", "LA Lakers":"LAL", "Miami":"MIA", 
+    team_name_transfer_dict_espn = {"LA Clippers" : "LAC", "San Antonio": "SAS", "Phoenix": "PHO", "Atlanta":"ATL", "Dallas":"DAL", "Portland":"POR", 
+                                    "Minnesota":"MIN", "New Orleans":"NOP", "Detroit":"DET", "Brooklyn":"BKN", "Toronto":"TOR", "LA Lakers":"LAL", "Miami":"MIA", 
                                     "Houston":"HOU", "Milwaukee":"MIL", "Charlotte":"CHA", "Boston":"BOS", "Philadelphia":"PHI", "Indiana":"IND", "Denver":"DEN", 
-                                    "Utah":"UTA", "Memphis":"MEM", "Washington":"WAS", "Golden State":"GS", "Chicago":"CHI", "Cleveland":"CLE", "New York":"NY", 
+                                    "Utah":"UTA", "Memphis":"MEM", "Washington":"WAS", "Golden State":"GSW", "Chicago":"CHI", "Cleveland":"CLE", "New York":"NYK", 
                                     "Oklahoma City":"OKC", "Orlando":"ORL", "Sacramento":"SAC"}
 
     team_stats_list = pd.read_html("http://www.espn.com/nba/hollinger/teamstats")
@@ -77,8 +151,21 @@ def adjust_fppg_by_pace(players_df):
 
 
 
-def import_contest_data(**kwargs):
+def import_contest_data(team_opp, inactive_players):
     players = pd.read_csv("Yahoo_DF_player_export.csv")
+
+    # convert team names from yahoo format to match with bball reference
+    team_name_transfer_dict_yahoo = {"NY": "NYK", "GS": "GSW", "NO": "NOP", "SA": "SAS"}   
+    players = players.replace({"Team": team_name_transfer_dict_yahoo})
+    
+    for i, player in players.iterrows():
+        if player.get("Injury Status") == "INJ" or player.get("Injury Status") == "O":
+            inactive_players[player.get("First Name") + ' ' + player.get("Last Name")] = 1
+        if player.get("Team") not in team_opp:
+            team_opp[player.get("Team")] = player.get("Opponent")
+   
+    return 
+    
 
     players["PG"] = (players["Position"] == 'PG').astype(float)
     players["SG"] = (players["Position"] == 'SG').astype(float)
